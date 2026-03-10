5d63f60b21e2ec3cd53496e40b071ab40164b28a
Alejandra <alejandrafernandezarauz@gmail.com>
2026-03-10 12:22:02 +0100

    feat: add regional referral tracking API routes for gostudy (Madrid, Barcelona, Mexico, Chile)

---
+++ api/gostudy-barcelona.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+    res.redirect(301, '/?utm_source=gostudy-barcelona&utm_medium=partner');
+}
---
+++ api/gostudy-chile.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+    res.redirect(301, '/?utm_source=gostudy-chile&utm_medium=partner');
+}
